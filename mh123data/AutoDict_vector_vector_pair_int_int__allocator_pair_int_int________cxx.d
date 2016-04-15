edac4df45490d7a2ccfe2927624802409f9f8332
CMS_pixel <j364y203@ku.edu>
2016-04-15 13:20:45 -0500

    removing results data stored in github

--- mh123data/AutoDict_vector_vector_pair_int_int__allocator_pair_int_int________cxx.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-AutoDict_vector_vector_pair_int_int__allocator_pair_int_int________cxx.so: /home/CMS_pixel/root/include/cintdictversion.h /home/CMS_pixel/root/include/RVersion.h
-AutoDict_vector_vector_pair_int_int__allocator_pair_int_int________cxx__ROOTBUILDVERSION= 5.32/04
